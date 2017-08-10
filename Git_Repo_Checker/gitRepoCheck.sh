91ba9dbffc1b0c9155c9c539b68a1cc9a815944b
jean-brice <jdickewu@gmail.com>
2017-08-10 16:40:34 +0000

    wrote first code of the git repo checker refs #2

---
+++ Git_Repo_Checker/gitRepoCheck.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+
+echo ----------------
+echo welcome to the git repo checker 
+echo ----------------
+echo ----------------
+echo ----------------
+echo 'what do you want to do ?'
+echo '
+    1. Check for changes in repo 
+    2. Push new changes 
+    3. Add new folder to check
+'
+
+
+#if 1 ask if you wnat to check all repo or just some ?
