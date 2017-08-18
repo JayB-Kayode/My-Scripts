94fb1e982482076a2de38a4f74770a2a0c73a358
jean-brice <jdickewu@gmail.com>
2017-08-18 10:29:57 +0000

    Updated the way to use root privileges if needed ! refs #4

--- easymover/easymover.sh
+++ easymover/easymover.sh
@@ -4,18 +4,31 @@ echo welcome to easymover
 echo ----------------
 echo this script helps you move your files
 echo ----------------
+echo '[IMPORTANT]
+  Please Run Me Inside the location of the file you want to move !
+  when running under root privileges be careful !'
+
+#
+
+
+
+
 #swicthing to root privileges 
 echo 'do you want/need to continue with root priveleges ? (y/n)'
 echo ----------------
 read privi 
 echo ----------------
 if [[ $privi == "y" ]]; then
-    echo enter your root password
+    echo enter your password
     echo ----------------
-    su -c "echo You now have root privileges; \
+    echo You now have root privileges
     echo 'please be carefull with whatever action you do
-     under root privileges'" -l
-      
+     under root privileges'
+   sudo whoami 
+else 
+
+       
 fi
+echo "
 
-
+"
