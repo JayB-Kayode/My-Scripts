ef0aa2c753eddea95b8a2a8fad00e642ee25eaa8
jean-brice <jdickewu@gmail.com>
2017-08-18 10:37:49 +0000

    initiated the bootme script project

---
+++ BootMe/bootMe.sh
@@ -0,0 +1,8 @@
+#! bin/bash
+
+#This is script is to create bootable USB image using bash terminal
+
+
+echo 'Welcome to BooTMe
+this script helps you easily burn ISO image unto
+USB drive Using Linux Terminal !'
